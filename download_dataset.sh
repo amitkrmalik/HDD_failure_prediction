e02ebf1690c7552fe43d6c7ba9d904abbcb26235
Amit Kumar <amimalik@cisco.com>
2024-08-13 23:05:48 +0530

    Updated the download script to download 4 Quarter dataset
    Q2 2024
    Q1 2024
    Q4 2023
    Q3 2023

--- download_dataset.sh
+++ download_dataset.sh
@@ -4,16 +4,20 @@ mkdir -p dataset
 cd dataset
 
 echo "downloading the dataset from the backblaze website"
+wget -nd -p https://f001.backblazeb2.com/file/Backblaze-Hard-Drive-Data/data_Q2_2024.zip
 wget -nd -p https://f001.backblazeb2.com/file/Backblaze-Hard-Drive-Data/data_Q1_2024.zip
+wget -nd -p https://f001.backblazeb2.com/file/Backblaze-Hard-Drive-Data/data_Q4_2023.zip
+wget -nd -p https://f001.backblazeb2.com/file/Backblaze-Hard-Drive-Data/data_Q3_2023.zip
+
 
 echo downloading complted..
 
 echo "unzipping the dataset"
+unzip data_Q2_2024.zip
 unzip data_Q1_2024.zip
+unzip data_Q4_2023.zip
+unzip data_Q3_2023.zip
 
 echo "remove metadata/stale directory"
 rm -rf __MACOSX/
 
-echo "creating a test data set for initial pre-processing testing"
-mkdir -p test
-cp data_Q1_2024/2024-0[1,2,3]-01.csv ./test/
