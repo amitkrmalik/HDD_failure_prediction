5c973ecec60472021b07797fc5b8eb5703a281d1
Amit Kumar <amimalik@cisco.com>
2024-08-15 11:24:56 +0530

    updated the download script to download 4 quarter files for analysis

--- download_dataset.sh
+++ download_dataset.sh
@@ -4,20 +4,44 @@ mkdir -p dataset
 cd dataset
 
 echo "downloading the dataset from the backblaze website"
-wget -nd -p https://f001.backblazeb2.com/file/Backblaze-Hard-Drive-Data/data_Q2_2024.zip
-wget -nd -p https://f001.backblazeb2.com/file/Backblaze-Hard-Drive-Data/data_Q1_2024.zip
-wget -nd -p https://f001.backblazeb2.com/file/Backblaze-Hard-Drive-Data/data_Q4_2023.zip
-wget -nd -p https://f001.backblazeb2.com/file/Backblaze-Hard-Drive-Data/data_Q3_2023.zip
 
+if [ ! -f data_Q2_2024.zip ]; then
+    wget -nd -p https://f001.backblazeb2.com/file/Backblaze-Hard-Drive-Data/data_Q2_2024.zip
+fi
+
+
+if [ ! -f data_Q1_2024.zip ]; then
+    wget -nd -p https://f001.backblazeb2.com/file/Backblaze-Hard-Drive-Data/data_Q1_2024.zip
+fi
+
+if [ ! -f data_Q4_2023.zip ]; then
+    wget -nd -p https://f001.backblazeb2.com/file/Backblaze-Hard-Drive-Data/data_Q4_2023.zip
+fi
+
+if [ ! -f data_Q3_2023.zip ]; then
+    wget -nd -p https://f001.backblazeb2.com/file/Backblaze-Hard-Drive-Data/data_Q3_2023.zip
+fi
+
+if [ ! -f data_Q2_2023.zip ]; then
+    wget -nd -p https://f001.backblazeb2.com/file/Backblaze-Hard-Drive-Data/data_Q2_2023.zip
+fi
 
 echo downloading complted..
 
 echo "unzipping the dataset"
-unzip data_Q2_2024.zip
-unzip data_Q1_2024.zip
-unzip data_Q4_2023.zip
-unzip data_Q3_2023.zip
+unzip -qq data_Q2_2024.zip
+unzip -qq data_Q1_2024.zip
+unzip -qq data_Q4_2023.zip
+unzip -qq data_Q3_2023.zip
+unzip -qq data_Q2_2023.zip
 
 echo "remove metadata/stale directory"
-rm -rf __MACOSX/
+find . -type d -name "__MACOSX" -exec rm -rf {} +
 
+# coallating the data into a single directory
+
+mkdir data
+mv data_Q2_2023/*.csv data
+mv data_Q3_2023/*.csv data
+mv data_Q4_2023/*.csv data
+mv data_Q1_2024/*.csv data
